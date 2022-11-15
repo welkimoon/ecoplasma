9d77bb63aa46d5c8c28adb0e5714bb0b8c8e616a
welkimoon <ivanova.margarita.2004@gmail.com>
2022-11-15 03:51:49 +0300

    ⠀

--- Практическая работа №1.py
+++ Практическая работа №1.py
@@ -1,16 +1,20 @@
 import random
 
-ver = 0
-ver2 = 0
+win_with_change = 0
+win_without_change = 0
 
 for i in range(100000):
     a = [0, 0, 1]
     b = random.choice(a)
     a.remove(b)
     if b == 1:
-        ver += 1
+        win_with_change += 1
     else:
         a.remove(0)
     if a[0] == 1:
-        ver2 += 1
-print(ver//1000, ver2//1000)
+        win_without_change += 1
+
+print(
+    f"""Шанс победы не меняя выбор: {win_with_change//1000}%
+Выбор менялся: {win_without_change//1000}%"""
+)
